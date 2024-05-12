d399bab6b0c9e549e3dfdf0b4dd7bcd5dca9683f
izzyjosh <calebayuba222@gmail.com>
2024-05-12 23:37:16 +0100

    updates

--- queue.js
+++ queue.js
@@ -1,52 +1,49 @@
 class Queue {
-	constructor (size) {
-		this.size = size
-		this.queue = []
-		this.first = 0
-		this.last = 0
-	}
+  constructor(size) {
+    this.size = size;
+    this.queue = [];
+    this.first = 0;
+    this.last = 0;
+  }
 
-	enqueue(data) {
-		if (this.last === this.size) {
-			console.log("Queue is full")
-		}
-		else {
-			this.queue.push(data)
-			this.last += 1
-		}
-	}
-	dequeue() {
-		if (this.first === this.last) {
-			console.log("Queue is empty")
-		}
-		else {
-			for (let i = 0;i < this.size; i++) {
-				if (i === this.last - 1) {
-					this.queue[i-1] = this.queue[i]
-				}
-				else {
-					this.queue[i] = this.queue[i+1]
-				}
-			}
-			this.last -= 1
-		}
-	}
-	display() {
-		if (this.first === this.last) {
-			console.log("Queue is Empty")
-		}
-		else {
-			for (let i = 0; i < this.last; i++) {
-				console.log(this.queue[i] + "<-- ")
-			}
-		}
-	}
+  enqueue(data) {
+    if (this.last === this.size) {
+      console.log("Queue is full");
+    } else {
+      this.queue.push(data);
+      this.last += 1;
+    }
+  }
+  dequeue() {
+    if (this.first === this.last) {
+      console.log("Queue is empty");
+    } else {
+      this.queue.map((item, i, queue) => {
+        if (i === this.last - 1) {
+          queue[i - 1] = queue[i];
+        } else {
+          queue[i] = queue[i + 1];
+        }
+      });
+      this.queue.pop()
+      this.last -= 1;
+    }
+  }
+  display() {
+    if (this.first === this.last) {
+      console.log("Queue is Empty");
+    } else {
+      this.queue.forEach(item => {
+        console.log(item + "<--");
+      });
+    }
+  }
 }
 
-const queue = new Queue(8)
-queue.display()
-queue.enqueue(8)
-queue.enqueue("mike")
-queue.display()
-queue.dequeue()
-queue.display()
+const queue = new Queue(8);
+queue.display();
+queue.enqueue(8);
+queue.enqueue("mike");
+queue.display();
+queue.dequeue();
+queue.display();
